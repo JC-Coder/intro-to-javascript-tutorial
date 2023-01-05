30de006851b63e9e28fb8d306f537ed3667197c3
JC-Coder <josephchimezie2003@gmail.com>
2023-01-05 16:51:43 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,77 @@
+// // this is how we write comments in javascript 
+
+
+// // variables
+// // var // global variable
+// // let // scoped based variable
+// // const // constant (values that are not meant to change)
+
+// // verb name = value;
+
+// var name = 'joseph';
+// let name2 = 'chiboy';
+// const name3 = 'david';
+
+// let buildingName = 'Yekini Estate'
+// let buildingPrice = '500,000 naira'
+
+// // console - debugging and testing 
+// console.log(buildingName)
+
+
+// // data types 
+// // strings = 'abcjdajfiajewojankavklajvajfgahf'
+// // numbers (integers) = 1234584784 
+// // floats = 1.345444
+// // boolean = true false
+
+
+// using a varible to get multiple instance of a story
+// let story = 'Hi my name is joseph , i am a dev, i work at a company called web dev computer company, my job is to build websites';
+
+// console.log(story);
+// console.log(story);
+// console.log(story);
+// console.log(story);
+// console.log(story);
+
+
+// operators 
+// 1. Assignment Operator (=)
+// 2. Arithmetic Operators (+, -, /, *) (works with numbers only)
+// 3. concatenation (+, works with only strings)
+
+// let add = 90 + 4;
+// let subtract = 55 - 5 - 10;
+// let divide = 50 / 5;
+// let multiplication = 20 * 10;
+
+// let calculate = 2 * 10 + 10 - 10 / 2;
+
+// console.log(add)
+// console.log(subtract)
+// console.log(divide)
+// console.log(multiplication)
+// console.log(calculate)
+
+
+// getting user input 
+// let userName = prompt('Enter your name')
+// console.log(userName);
+
+
+// writing a story 
+// let userName = prompt('enter your name');
+
+// let story = 'Hi my name is ' + userName + ' , i am a dev, i work at a company called web dev computer company, my job is to build websites';
+
+// console.log(story);
+
+
+// simple calculator in javascript
+
+let num1 = prompt('enter first number');
+let num2 = prompt('enter second number');
+
+let result = parseInt(num1) + parseInt(num2);
+console.log(result);
